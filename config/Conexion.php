dec4760be71c9c0e9774e35194c484b19bd32834
suso-sky <pipeprocampo@gmail.com>
2023-06-25 17:32:15 -0500

    a

--- config/Conexion.php
+++ config/Conexion.php
@@ -2,15 +2,11 @@
  class CConexion{
     public static function ConexionBD(){
         $host='localhost';
-<<<<<<< HEAD
-        $dbname='dbphp';
-        $username='juanito';
-        $pasword='123456';
-=======
+
         $dbname='notas';
         $username='postgres';
         $pasword='postgres';
->>>>>>> 66d682c (a)
+
     try{
         $conn = new PDO("pgsql:host=$host; dbname=$dbname", $username, $pasword);
     }catch(PDOException $exp){
